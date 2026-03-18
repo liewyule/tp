129535c6ef486dbfb5f786b7b044a7dcb7ce695d
Liew Yu Le <liewyule05@gmail.com>
2026-03-18 17:18:28 +0800

    Add copy url functionality

---
+++ src/main/java/seedu/address/logic/commands/CopyCommand.java
@@ -0,0 +1,87 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import java.util.Optional;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.application.Application;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import seedu.address.model.application.Url;
+
+/**
+ * Deletes a application identified using it's displayed index from the address book.
+ */
+public class CopyCommand extends Command {
+
+    public static final String COMMAND_WORD = "copy";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Copys the application URL identified by the index number used in the displayed application list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_COPY_APPLICATION_SUCCESS = "Copy Application URL: %1$s";
+
+    private final Index targetIndex;
+
+    public CopyCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Application> lastShownList = model.getFilteredApplicationList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_APPLICATION_DISPLAYED_INDEX);
+        }
+
+        Application applicationToCopy = lastShownList.get(targetIndex.getZeroBased());
+
+        Optional<Url> optionalUrl = applicationToCopy.getUrl();
+
+        if (optionalUrl.isEmpty()) {
+            throw new CommandException("This application does not have a URL to copy.");
+        }
+
+        String urlString = optionalUrl.get().toString();
+
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        content.putString(urlString);
+        clipboard.setContent(content);
+
+        return new CommandResult(String.format(MESSAGE_COPY_APPLICATION_SUCCESS,
+                Messages.format(applicationToCopy)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof CopyCommand)) {
+            return false;
+        }
+
+        CopyCommand otherCopyCommand = (CopyCommand) other;
+        return targetIndex.equals(otherCopyCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -11,6 +11,7 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CopyCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
@@ -76,7 +77,8 @@ public class AddressBookParser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
-
+        case CopyCommand.COMMAND_WORD:
+            return new CopyCommandParser().parse(arguments);
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
