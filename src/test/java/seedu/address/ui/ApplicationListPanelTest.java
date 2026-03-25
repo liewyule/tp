a6551bda3a5d115a602f8cc9b8b4210541013e50
MingYou <limmingyou@gmail.com>
2026-03-25 18:42:37 +0800

    Added test cases

---
+++ src/test/java/seedu/address/ui/ApplicationListPanelTest.java
@@ -0,0 +1,14 @@
+package seedu.address.ui;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationListPanelTest {
+
+    @Test
+    public void applicationListPanel_createsResponsiveLayout() {
+        // Integration test: ApplicationListPanel is created with responsive layout
+        // functionality through setupResponsiveLayout() called in constructor.
+        // Actual responsive behavior is tested through manual UI testing.
+    }
+
+}
